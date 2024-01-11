b6762854dd680e61b4562eb81e6bb26f61c4ee43
Nandy365 <kumarnan217@gmail.com>
2024-01-11 21:33:20 +0530

    Arithmetic operators

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//VARIABLE a container that stores value
+
+
 // let thisname = 'nandy';
 // let price = 3123;
 // let gst = 10.1;
@@ -13,10 +16,40 @@
 // console.log(`now offline${whatsapp}`)
 // console.log(`now online ${insta}`)
 
-let myname ="nandhakumar"
-let age = 23;
-let myland ="london"
+// let myname ="nandhakumar"
+// let age = 23;
+// let myland ="london"
 
-document.getElementById("p1").textContent = `my name ${myname}`;
-document.getElementById("p2").textContent = `my age ${age}`;
-document.getElementById("p3").textContent = `my location ${myland}`;
+// document.getElementById("p1").textContent = `my name ${myname}`;
+// document.getElementById("p2").textContent = `my age ${age}`;
+// document.getElementById("p3").textContent = `my location ${myland}`;
+
+let amount = 40
+
+// amount = amount +1
+// amount = amount -1
+// amount = amount *1
+// amount = amount /4
+// amount = amount **2
+
+// amount += 1
+//amount -= 1
+//amount *= 5
+// amount /= 1
+// amount **= 1
+//amount++
+//amount--
+
+
+//console.log(amount)
+/*
+operator precedence
+1 parenthesis ()
+2 exponents 
+3 multiplications & division & modulo
+4 addition & subtraction
+*/
+
+let result = 2 + 3 - 4 * 3 % 4
+
+console.log(result)
