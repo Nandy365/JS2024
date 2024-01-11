010bad56322dba21542f1e14401d4bb37d80370b
Nandy365 <kumarnan217@gmail.com>
2024-01-11 14:45:08 +0530

    variable

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
- 
+// let thisname = 'nandy';
+// let price = 3123;
+// let gst = 10.1;
+
+// console.log(`name of the product${thisname}`)
+// console.log(`totall cost of the thing ${price}`)
+// console.log(`totall tax${gst}`)
+// let facebook = true
+// let whatsapp = false
+// let insta = true
+
+// console.log(`now online ${facebook}`)
+// console.log(`now offline${whatsapp}`)
+// console.log(`now online ${insta}`)
+
+let myname ="nandhakumar"
+let age = 23;
+let myland ="london"
+
+document.getElementById("p1").textContent = `my name ${myname}`;
+document.getElementById("p2").textContent = `my age ${age}`;
+document.getElementById("p3").textContent = `my location ${myland}`;
